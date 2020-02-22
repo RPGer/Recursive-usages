ab6cb78678bb4c1c3dcfeb34d5b3d92e9a77eccb
RPGer <rpgerko@gmail.com>
2020-02-23 00:54:46 +0100

    Laravel routes WIP

--- src/main/java/myToolWindow/Nodes/CodeNodeFactory.java
+++ src/main/java/myToolWindow/Nodes/CodeNodeFactory.java
@@ -1,8 +1,15 @@
 package myToolWindow.Nodes;
 
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Query;
 import com.jetbrains.php.lang.documentation.phpdoc.psi.impl.tags.PhpDocTagImpl;
 import com.jetbrains.php.lang.psi.elements.impl.MethodImpl;
+import com.jetbrains.php.lang.psi.elements.impl.MethodReferenceImpl;
+import com.jetbrains.php.lang.psi.elements.impl.VariableImpl;
 
 import static com.intellij.testIntegration.TestFinderHelper.isTest;
 
@@ -18,10 +25,32 @@ public class CodeNodeFactory {
     }
 
     private static boolean isRoute(MethodImpl mel) {
+        return isSymfonyRouteDefinedByAnnotation(mel) || isLaravelRoute(mel);
+    }
+
+    private static boolean isSymfonyRouteDefinedByAnnotation(MethodImpl mel) {
         PsiElement[] annotations = mel.getNode().getTreePrev().getTreePrev().getPsi().getChildren();
         for (PsiElement annotation : annotations) {
             if (annotation instanceof PhpDocTagImpl && ((PhpDocTagImpl) annotation).getName().equals("@Route")) {
                 return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean isLaravelRoute(MethodImpl element) {
+        Query<PsiReference> q = ReferencesSearch.search(element);
+
+        for (PsiReference r : q) {
+            PsiElement el = r.getElement();
+            PsiFile file = el.getContainingFile();
+            final int offset = el.getTextOffset();
+
+            MethodReferenceImpl mel = PsiTreeUtil.findElementOfClassAtOffset(file, offset, MethodReferenceImpl.class, false);
+
+            if (mel != null) {
+                //VariableImpl v = (VariableImpl) el.getFirstChild();
             }
         }
 
